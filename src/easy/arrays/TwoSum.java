56d8ff1659e33432fb6f2ef3a227262503ff4806
Ram <80216696+ramky3757@users.noreply.github.com>
2024-08-12 22:57:35 +0530

    Updated Code with bruteForce

--- src/easy/arrays/TwoSum.java
+++ src/easy/arrays/TwoSum.java
@@ -19,4 +19,17 @@ public class TwoSum {
         }
         System.out.println("no matching key");
     }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for(int p1=0; p1<nums.length; p1++){
+            int numberToFind = target - nums[p1];
+
+            for(int p2=p1+1;p2<nums.length;p2++){
+                if(numberToFind == nums[p2]){
+                    return new int[]{p1,p2};
+                }
+            }
+        }
+        return null;
+    }
 }
